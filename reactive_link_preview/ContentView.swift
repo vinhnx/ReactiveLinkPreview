ceeca6a24a113b4f439963f33862b4b79311fe00
Vinh Nguyen <vinhnguyen2308@gmail.com>
2019-06-06 20:30:55 +0700

    Initial Commit

---
+++ reactive_link_preview/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  reactive_link_preview
+//
+//  Created by Vinh Nguyen on 6/6/19.
+//  Copyright © 2019 Vinh Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ reactive_link_previewTests/reactive_link_previewTests.swift
@@ -0,0 +1,34 @@
+//
+//  reactive_link_previewTests.swift
+//  reactive_link_previewTests
+//
+//  Created by Vinh Nguyen on 6/6/19.
+//  Copyright © 2019 Vinh Nguyen. All rights reserved.
+//
+
+import XCTest
+@testable import reactive_link_preview
+
+class reactive_link_previewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
